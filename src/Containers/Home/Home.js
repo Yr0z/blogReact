ee6f27805ca311d939b084b9cdb3a8eb9a14dad6
Yr0z <y.cocheteau@gmail.com>
2021-05-29 15:25:25 +0200

    include React Link

--- src/Containers/Home/Home.js
+++ src/Containers/Home/Home.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Home() {
-    return <h1>Accueil</h1>;
+    return (
+        <>
+            <h1>Accueil</h1>
+            <Link to="/contact" style={{ marginRight: '10px' }}>
+                Contact
+            </Link>
+            <Link to="/articles">Articles</Link>
+        </>
+    );
 }
 
 export default Home;
