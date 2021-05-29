057739a742cc5bcefe69407ef0b4f71675a1ab1c
Yr0z <y.cocheteau@gmail.com>
2021-05-29 15:18:14 +0200

    start route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 // Librairies
 import React from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 
 // Composants
 import Layout from './hoc/Layout/Layout';
+import Home from './Containers/Home/Home';
+import Contact from './Components/Contact/Contact';
+import Articles from './Containers/Articles/Articles';
 
 function App() {
     return (
         <div className="App">
-            <Layout></Layout>
+            <Layout>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/contact" component={Contact} />
+                    <Route exact path="/articles" component={Articles} />
+                </Switch>
+            </Layout>
         </div>
     );
 }
